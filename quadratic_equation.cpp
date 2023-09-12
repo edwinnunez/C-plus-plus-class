f2bea7dd88c1e4937bf91ce964a0c3cf577ee2b5
edwinnunez <edwinnunez2003@gmail.com>
2023-09-12 13:41:29 -0400

    modified:   quadratic_equation.cpp

--- quadratic_equation.cpp
+++ quadratic_equation.cpp
@@ -10,7 +10,7 @@ double a, b, c, sol1, sol2;
 cout<<"Please provide the coefficients of your quadratic equation: "; 
 cin >> a >> b >> c;
 
-sol1 = (-b + sqrt(pow(b,3)-4*c*a))/(2*a);
+sol1 = (-b + sqrt(pow(b,2)-4*c*a))/(2*a);
 sol2 = (-b - sqrt(pow(b,2)-4*c*a))/(2*a);
 
 cout<<"Solution 1: "<<sol1<<endl;
